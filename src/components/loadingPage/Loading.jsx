bca7672fee1600e13d97398f7cee3a44b3551f97
Dagson Gabriel <dagson.g.01@gmail.com>
2024-11-18 14:37:47 -0300

    feat: implementa lazy loading junto de tela de carregamento

---
+++ src/components/loadingPage/Loading.jsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+
+function GradientCircularProgress() {
+  return (
+    <React.Fragment>
+      <svg width={0} height={0}>
+        <defs>
+          <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" stopColor="#e01cd5" />
+            <stop offset="100%" stopColor="#1CB5E0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <CircularProgress sx={{ 'svg circle': { stroke: 'url(#my_gradient)' } }} />
+    </React.Fragment>
+  );
+}
+export default function LoadingBar() {
+  return (
+    <div style={{ height: 'calc(100vh - 64px)', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <GradientCircularProgress />
+    </div>
+  );
+}
