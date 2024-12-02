7189f8f11cb13de80d1f8ac37ed61e48c3bda6ad
Dagson Gabriel <dagson.g.01@gmail.com>
2024-12-02 03:09:27 -0300

    refactor: conserta identação

--- src/pages/forum/Forum.jsx
+++ src/pages/forum/Forum.jsx
@@ -93,7 +93,7 @@ export default function Forum() {
       [id]: !prev[id],
     }));
   };
-  const handleOpenModal = () => { 
+  const handleOpenModal = () => {
     setOpen(true);
   };
 
@@ -106,207 +106,207 @@ export default function Forum() {
 
   const handleReport = (event) => {
     event.preventDefault()
-    if(!event.target.comment.value){
+    if (!event.target.comment.value) {
       toast.warn('Comentário vazio')
-    }else{
+    } else {
       toast.success('Comentário enviado com sucesso')
     }
   }
 
   return (
     <>
-    <ToastContainer 
-        position="bottom-right" 
-        autoClose={3000} 
-        closeOnClick={true} 
-        pauseOnHover={true} 
-        draggable={true} 
+      <ToastContainer
+        position="bottom-right"
+        autoClose={3000}
+        closeOnClick={true}
+        pauseOnHover={true}
+        draggable={true}
         theme="colored"
-        />
-    <Container maxWidth="md" sx={{ marginTop: 4 }}>
-      <TopicModal open={open} handleClose={handleClose} />
-      <ButtonNewTopic onClick={handleOpenModal}>
-        <Typography variant="body" sx={{ color: "#013A93" }}>
-          Criar Novo Tópico
-        </Typography>
-        <SendIcon color="primary"></SendIcon>
-      </ButtonNewTopic>
-      {posts.map((post) => (
-        <Paper
-          key={post.id}
-          sx={{
-            padding: 2,
-            marginBottom: 2,
-            border: "1px solid #013A93",
-            borderRadius: 2,
-            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-          }}
-        >
-          <Box display="flex" flexDirection="column">
-            <Box display="flex">
-              <Box
-                display="flex"
-                alignItems="center"
-                flexDirection="column"
-                mt={1}
-              >
-                <IconButton
-                  color="primary"
-                  onClick={() => handleUpvote(post.id)}
+      />
+      <Container maxWidth="md" sx={{ marginTop: 4 }}>
+        <TopicModal open={open} handleClose={handleClose} />
+        <ButtonNewTopic onClick={handleOpenModal}>
+          <Typography variant="body" sx={{ color: "#013A93" }}>
+            Criar Novo Tópico
+          </Typography>
+          <SendIcon color="primary"></SendIcon>
+        </ButtonNewTopic>
+        {posts.map((post) => (
+          <Paper
+            key={post.id}
+            sx={{
+              padding: 2,
+              marginBottom: 2,
+              border: "1px solid #013A93",
+              borderRadius: 2,
+              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <Box display="flex" flexDirection="column">
+              <Box display="flex">
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  flexDirection="column"
+                  mt={1}
                 >
-                  <FontAwesomeIcon icon={faArrowUp} />
-                </IconButton>
-                <Typography variant="body2" sx={{ mx: 1 }}>
-                  {post.votes}
-                </Typography>
-                <IconButton
-                  color="primary"
-                  onClick={() => handleDownvote(post.id)}
-                >
-                  <FontAwesomeIcon icon={faArrowDown} />
-                </IconButton>
-              </Box>
-              <Box>
-                <Typography variant="caption" color="primary" ml={2}>
-                  {post.date}
-                </Typography>
-                <Typography
-                  variant="body1"
-                  ml={2}
-                  sx={{
-                    color: "#013A93",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {post.text}
-                </Typography>
-                <Box display="flex" alignItems="center" mt={2} ml={2} gap={2}>
-                  <Box display="flex">
-                    <IconButton>
-                      <ChatBubbleOutlineIcon color="primary" onClick={() => handleTextShow(post.id)}/>
-                      <Typography variant="body2" color="primary">
-                        {post.comments}
-                      </Typography>
-                    </IconButton>
-                  </Box>
-                  <IconButton>
-
-                  <ReportGmailerrorredIcon color="primary" />
+                  <IconButton
+                    color="primary"
+                    onClick={() => handleUpvote(post.id)}
+                  >
+                    <FontAwesomeIcon icon={faArrowUp} />
+                  </IconButton>
+                  <Typography variant="body2" sx={{ mx: 1 }}>
+                    {post.votes}
+                  </Typography>
+                  <IconButton
+                    color="primary"
+                    onClick={() => handleDownvote(post.id)}
+                  >
+                    <FontAwesomeIcon icon={faArrowDown} />
                   </IconButton>
                 </Box>
-              </Box>
-              {visibleComments[post.id] && post.comments > 0 ? (
-                <KeyboardArrowUpIcon
-                  sx={{
-                    ml: "auto",
-                    mt: "auto",
-                    cursor: "pointer",
-                    transition: "all 0.3s ease",
-                    "&:hover": {
-                      backgroundColor: "#e0e0e0",
-                      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-                      transform: "scale(1.01)",
-                    },
-                    display: "flex",
-                  }}
-                  onClick={() => handleComentShow(post.id)}
-                  color="primary"
-                />
-              ) : (
-                <KeyboardArrowDownIcon
-                  sx={{
-                    ml: "auto",
-                    mt: "auto",
-                    cursor: "pointer",
-                    transition: "all 0.3s ease",
-                    "&:hover": {
-                      backgroundColor: "#e0e0e0",
-                      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-                      transform: "scale(1.01)",
-                    },
-                    display: "flex",
-                  }}
-                  onClick={() => handleComentShow(post.id)}
-                  color="primary"
-                />
-              )}
-            </Box>
-            <Box>
-              {visibleComments[post.id] && post.comments > 0 && (
-                <>
-                  <Box
+                <Box>
+                  <Typography variant="caption" color="primary" ml={2}>
+                    {post.date}
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    ml={2}
                     sx={{
-                      border: "1px solid #013A93",
-                      borderRadius: 2,
-                      padding: 1,
-                      marginTop: 4,
-                      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+                      color: "#013A93",
+                      fontWeight: "bold",
                     }}
                   >
+                    {post.text}
+                  </Typography>
+                  <Box display="flex" alignItems="center" mt={2} ml={2} gap={2}>
                     <Box display="flex">
-                      <Avatar
-                        src="https://super.abril.com.br/wp-content/uploads/2018/07/5281183b865be245b1000225gorila.jpeg?quality=70&w=720&crop=1"
-                        sx={{
-                          width: 15,
-                          height: 15,
-                          marginRight: 1,
-                          ml: 1,
-                        }}
-                      />
-                      <Typography
-                        variant="caption"
-                        sx={{
-                          color: "#013A93",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        {Users.find((user) => user.id === 1).name}
-                      </Typography>
-                      <Typography
-                        color="primary"
-                        variant="caption"
-                        sx={{
-                          ml: 1,
-                        }}
-                      >
-                        {Users.find((user) => user.id === 1).date}
-                      </Typography>
-                      <ReportGmailerrorredIcon
-                        color="primary"
-                        sx={{
-                          ml: "auto",
-                          cursor: "pointer",
-                          transition: "all 0.3s ease",
-                          "&:hover": {
-                            backgroundColor: "#e0e0e0",
-                            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-                            transform: "scale(1.01)",
-                          },
-                        }}
-                      />
+                      <IconButton>
+                        <ChatBubbleOutlineIcon color="primary" onClick={() => handleTextShow(post.id)} />
+                        <Typography variant="body2" color="primary">
+                          {post.comments}
+                        </Typography>
+                      </IconButton>
                     </Box>
-                    <Box mt={1} ml={2}>
-                      <Typography
-                        variant="body2"
-                        fontWeight="bold"
-                        sx={{
-                          color: "#013A93",
-                        }}
-                      >
-                        Comentário teste
-                      </Typography>
-                    </Box>
+                    <IconButton>
+
+                      <ReportGmailerrorredIcon color="primary" />
+                    </IconButton>
                   </Box>
-                </>
-              )}
-              {visibleText[post.id] && (
+                </Box>
+                {visibleComments[post.id] && post.comments > 0 ? (
+                  <KeyboardArrowUpIcon
+                    sx={{
+                      ml: "auto",
+                      mt: "auto",
+                      cursor: "pointer",
+                      transition: "all 0.3s ease",
+                      "&:hover": {
+                        backgroundColor: "#e0e0e0",
+                        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+                        transform: "scale(1.01)",
+                      },
+                      display: "flex",
+                    }}
+                    onClick={() => handleComentShow(post.id)}
+                    color="primary"
+                  />
+                ) : (
+                  <KeyboardArrowDownIcon
+                    sx={{
+                      ml: "auto",
+                      mt: "auto",
+                      cursor: "pointer",
+                      transition: "all 0.3s ease",
+                      "&:hover": {
+                        backgroundColor: "#e0e0e0",
+                        boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+                        transform: "scale(1.01)",
+                      },
+                      display: "flex",
+                    }}
+                    onClick={() => handleComentShow(post.id)}
+                    color="primary"
+                  />
+                )}
+              </Box>
+              <Box>
+                {visibleComments[post.id] && post.comments > 0 && (
+                  <>
                     <Box
-                      mt={2}
                       sx={{
+                        border: "1px solid #013A93",
+                        borderRadius: 2,
+                        padding: 1,
+                        marginTop: 4,
                         boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
                       }}
                     >
-                      <form onSubmit={handleReport}>
+                      <Box display="flex">
+                        <Avatar
+                          src="https://super.abril.com.br/wp-content/uploads/2018/07/5281183b865be245b1000225gorila.jpeg?quality=70&w=720&crop=1"
+                          sx={{
+                            width: 15,
+                            height: 15,
+                            marginRight: 1,
+                            ml: 1,
+                          }}
+                        />
+                        <Typography
+                          variant="caption"
+                          sx={{
+                            color: "#013A93",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          {Users.find((user) => user.id === 1).name}
+                        </Typography>
+                        <Typography
+                          color="primary"
+                          variant="caption"
+                          sx={{
+                            ml: 1,
+                          }}
+                        >
+                          {Users.find((user) => user.id === 1).date}
+                        </Typography>
+                        <ReportGmailerrorredIcon
+                          color="primary"
+                          sx={{
+                            ml: "auto",
+                            cursor: "pointer",
+                            transition: "all 0.3s ease",
+                            "&:hover": {
+                              backgroundColor: "#e0e0e0",
+                              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+                              transform: "scale(1.01)",
+                            },
+                          }}
+                        />
+                      </Box>
+                      <Box mt={1} ml={2}>
+                        <Typography
+                          variant="body2"
+                          fontWeight="bold"
+                          sx={{
+                            color: "#013A93",
+                          }}
+                        >
+                          Comentário teste
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </>
+                )}
+                {visibleText[post.id] && (
+                  <Box
+                    mt={2}
+                    sx={{
+                      boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+                    }}
+                  >
+                    <form onSubmit={handleReport}>
                       <TextField
                         id="comment"
                         fullWidth
@@ -316,19 +316,19 @@ export default function Forum() {
                         InputProps={{
                           endAdornment: (
                             <IconButton type="submit">
-                              <SendIcon color="primary"/>
+                              <SendIcon color="primary" />
                             </IconButton>
                           ),
                         }}
                       />
-                      </form>
-                    </Box>
-                  )}
+                    </form>
+                  </Box>
+                )}
+              </Box>
             </Box>
-          </Box>
-        </Paper>
-      ))}
-    </Container>
+          </Paper>
+        ))}
+      </Container>
     </>
   );
 }
