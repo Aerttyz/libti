7256f2bc8d935e3b00b0a8daf465680e6a78ea96
Dagson Gabriel <dagson.g.01@gmail.com>
2024-12-02 08:25:21 -0300

    feat: atualiza drawer com autenticação

--- src/components/navbar/sections/Drawer.jsx
+++ src/components/navbar/sections/Drawer.jsx
@@ -1,53 +1,82 @@
 import React from 'react';
-import { 
-  Box, 
-  Drawer, 
-  List, 
-  Divider, 
-  ListItem, 
-  ListItemButton, 
-  ListItemIcon, 
+import {
+  Box,
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
   IconButton,
   Typography
 } from '@mui/material';
-import { 
+import {
   Menu as MenuIcon,
-  Home, 
-  CloudUpload, 
+  Home,
+  CloudUpload,
   CloudDownload,
   Forum,
   MailOutline,
   Login,
+  Logout,
+  AccountCircle,
   PersonAddOutlined,
   Close
 } from '@mui/icons-material';
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../../contexts/AuthContext';
 
 const Icons = (Icon) => {
   return <Icon sx={{ ml: '10px', color: 'white' }} />;
 }
 
+const ItemsMenu = (props) => {
+  const { text, logout, toggleDrawer } = props;
+  return (
+    <React.Fragment key={text.name}>
+      <NavLink
+        key={text.name}
+        to={text.url}
+        style={({ isActive }) => ({
+          backgroundColor: isActive ? '#0045AF' : '',
+          textDecoration: 'none',
+          display: 'flex',
+        })}
+      >
+        <ListItem disablePadding>
+          <ListItemButton
+            sx={{ '&:hover': { backgroundColor: '#6899E6' } }}
+            onClick={text.name === 'sair' ? logout : toggleDrawer(false)}
+          >
+            <ListItemIcon>
+              {text.icon}
+            </ListItemIcon>
+            <Typography variant="h6" sx={{ color: 'white', fontFamily: 'Roboto', fontSize: 14, m: '5px 0' }}>{text.name.toUpperCase()}</Typography>
+          </ListItemButton>
+        </ListItem>
+      </NavLink>
+    </React.Fragment>
+  );
+}
+
 const pages = [
-  { name: 'home', url: '/' }, 
-  { name: 'upload', url: '/upload' }, 
-  { name: 'requests', url: '/requests' }, 
-  { name: 'fórum', url: '/forum' }, 
-  { name: 'contato', url: '/contato' }, 
-  { name: 'login', url: '/login' }, 
-  { name: 'criar conta', url: '/cadastro' }
+  { name: 'home', url: '/', role: 'all', icon: Icons(Home) },
+  { name: 'upload', url: '/upload', role: 'user', icon: Icons(CloudUpload) },
+  { name: 'requests', url: '/requests', role: 'admin', icon: Icons(CloudDownload) },
+  { name: 'fórum', url: '/forum', role: 'user', icon: Icons(Forum) },
+  { name: 'contato', url: '/contato', role: 'all', icon: Icons(MailOutline) },
 ];
-const icons = [
-  Icons(Home),
-  Icons(CloudUpload),
-  Icons(CloudDownload),
-  Icons(Forum),
-  Icons(MailOutline),
-  Icons(Login),
-  Icons(PersonAddOutlined),
+
+const pagesAuth = [
+  { name: 'login', url: '/login', role: 'all', icon: Icons(Login) },
+  { name: 'cadastro', url: '/cadastro', role: 'all', icon: Icons(PersonAddOutlined) },
+  { name: 'perfil', url: '/perfil', role: 'user', icon: Icons(AccountCircle) },
+  { name: 'sair', url: '/logout', role: 'user', icon: Icons(Logout) },
 ];
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = React.useState(false);
+  const { isAuthenticated, logout } = useAuth();
 
   const toggleDrawer = (newOpen) => () => {
     setOpen(newOpen);
@@ -56,38 +85,19 @@ export default function TemporaryDrawer() {
   const DrawerList = (
     <Box sx={{ width: 250, height: '100%', backgroundColor: '#013A93', color: 'white', overflow: 'scroll' }} role="presentation">
       <Box sx={{ display: 'flex', justifyContent: 'flex-end', padding: '10px' }}>
-        <Close onClick={toggleDrawer(false)} sx={{ '&:hover': { backgroundColor: '#6899E6'} }} />
+        <Close onClick={toggleDrawer(false)} sx={{ '&:hover': { backgroundColor: '#6899E6' } }} />
       </Box>
       <List>
-        {pages.map((text, index) => (
-          <React.Fragment key={text.name}>
-            <NavLink 
-              key={text.name}
-              to={text.url}
-              style={({isActive}) => ({
-                backgroundColor: isActive ? '#0045AF' : '',
-                textDecoration: 'none',
-                display: 'flex',
-              })}
-            >
-              <ListItem disablePadding>
-                <ListItemButton
-                  sx={{ '&:hover': { backgroundColor: '#6899E6'} }}
-                  onClick={toggleDrawer(false)}
-                >
-                  <ListItemIcon>
-                    {icons[index]}
-                  </ListItemIcon>
-                  <Typography variant="h6" sx={{ color: 'white', fontFamily: 'Roboto',  fontSize: 14, m: '5px 0' }}>{text.name.toUpperCase()}</Typography>
-                </ListItemButton>
-              </ListItem>
-            </NavLink>
-            {index === 4 && <Divider sx={{ margin: '10px', backgroundColor: 'lightgray' }} />}
-          </React.Fragment>
+        {pages.filter(page => !isAuthenticated ? page.role !== 'admin' : page).map((text, index) => (
+          <ItemsMenu text={text} key={index} logout={logout} toggleDrawer={toggleDrawer} />
+        ))}
+        <Divider sx={{ margin: '10px', backgroundColor: 'lightgray' }} />
+        {pagesAuth.filter(page => isAuthenticated ? page.role === 'user' : page.role === 'all').map((text, index) => (
+          <ItemsMenu text={text} key={index} logout={logout} toggleDrawer={toggleDrawer} />
         ))}
       </List>
     </Box>
-  );  
+  );
 
   return (
     <div>
@@ -99,7 +109,7 @@ export default function TemporaryDrawer() {
         color="inherit"
         onClick={toggleDrawer(true)}
       >
-        <MenuIcon sx={{ color: 'white' }}/>
+        <MenuIcon sx={{ color: 'white' }} />
       </IconButton>
       <Drawer open={open} onClose={toggleDrawer(false)}>
         {DrawerList}
